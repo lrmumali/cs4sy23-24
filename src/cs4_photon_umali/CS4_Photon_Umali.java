8290812b79ae373c9522012ee80a373e3c2b28c8
PHOTON <PHOTON@172.22.16.92>
2023-09-05 15:54:14 +0800

    Ex1

--- src/cs4_photon_umali/CS4_Photon_Umali.java
+++ src/cs4_photon_umali/CS4_Photon_Umali.java
@@ -15,8 +15,50 @@ public class CS4_Photon_Umali {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("greeting");
+        String ob1 = "Item 1";
+    String ob2 = "Item 2";
+    String ob3 = "Item 3";
+
+    String ob1Name = "lightbulb";
+    String ob2Name = "electric fan";
+    String ob3Name = "cellphone";
+
+    int ob1Wattage = 18;
+    int ob2Wattage = 50;
+    int ob3Wattage = 5;
+
+    String ob1Color = "white";
+    String ob2Color = "blue";
+    String ob3Color = "black";
+    
+    
+    
+
+    
+    System.out.println(ob1);
+    System.out.println("Name: " + ob1Name);
+    System.out.println("Wattage in watts: " + ob1Wattage);
+    System.out.println("Color: " + ob1Color);
+
+    System.out.println("");
+
+    System.out.println(ob2);
+    System.out.println("Name: " + ob2Name);
+    System.out.println("Wattage in watts: " + ob2Wattage);
+    System.out.println("Color: " + ob2Color);
+
+    System.out.println("");
+
+    System.out.println(ob3);
+    System.out.println("Name: " + ob3Name);
+    System.out.println("Wattage in watts: " + ob3Wattage);
+    System.out.println("Color: " + ob3Color);
+
+    System.out.println("");
+
+    System.out.println("Total Wattage: " + (ob1Wattage + ob2Wattage + ob3Wattage));
+    System.out.println("Electric fan uses more electricity than cellphone: " + (ob2Wattage > ob3Wattage));
+    System.out.println("Cellphone and lightbulb have the same color: " + (ob1Color == ob3Color));
     }
     
 }
